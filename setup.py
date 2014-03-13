87cbce137ab7987060079cf975ff6bc29f7a006d
Ben Pellow <benjamin.pellow@gmail.com>
2014-03-12 18:51:30 -0700

    addons -> addonlist :sparkles:

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-    name = 'addons',
-    packages = ['addons'], # this must be the same as the name above
+    name = 'addonlist',
+    packages = ['addonlist'], # this must be the same as the name above
     version = '0.0.1',
-    description = 'Addons package placeholder',
+    description = 'AddonList Addons package placeholder',
     author = 'Ben Pellow',
     author_email = 'benjamin.pellow@gmail.com',
     url = 'https://github.com/addonlist/addons-pypi',   # use the URL to the github repo
     download_url = 'https://github.com/addonlist/addons-pypi/tarball/0.0.1', # download tar package, could be binary
-    keywords = ['addons', 'startup', 'email', 'analytics'], # keywords
+    keywords = ['addonlist', 'addons', 'startup', 'email', 'analytics'], # keywords
     classifiers = ["Development Status :: 1 - Planning"],
 )
